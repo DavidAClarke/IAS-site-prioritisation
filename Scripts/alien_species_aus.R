24910c9ca127d5e7e584e48cf087f1a6070dfd79
David Clarke <david_anthony_clarke@hotmail.com>
2021-07-05 17:03:09 +1000

    Update alien_species_aus.R

--- Scripts/alien_species_aus.R
+++ Scripts/alien_species_aus.R
@@ -3,30 +3,30 @@ aliens_Aus <- read.csv("C:/Users/dcla0008/Dropbox/PhD/Thesis/Data/Data general/h
 aliens_Aus_taxalist <- read.csv("C:/Users/dcla0008/Dropbox/PhD/Thesis/Data/Data general/hseebens-sTwist_Workflow-7b32cf8/Output/SInAS_AlienSpeciesDB_2.3.1_FullTaxaList.csv", sep = " ")
 
 #Unique names
-#alien_names <- unique(aliens_Aus_taxalist$species)
+alien_names <- unique(aliens_Aus_taxalist$species)
 
 #Further harmonization (does this get me the same as aliens_Aus_taxalist$species?)
+#yes it does. Therefore, not necessary to do this. just use aliens_Aus_taxalist$species
 # AlienTaxInfo <- traitdataform::get_gbif_taxonomy(alien_names) #function from traitdataform package
 # write.csv(AlienTaxInfo, file = file.path("SpeciesData", "AlienTaxInfo.csv"))
-AlienTaxInfo <- read.csv(file.path("SpeciesData", "AlienTaxInfo.csv"))
-AlienTaxInfo <- as_tibble(AlienTaxInfo)
-AlienTaxInfo_names <- AlienTaxInfo %>% 
-  distinct(verbatimScientificName = verbatimScientificName, .keep_all = T) %>%
-  rename(acceptedName = "scientificName") %>%
-  rename(Taxon = "verbatimScientificName") %>%
-  dplyr::select(Taxon, acceptedName)
-aliens_Aus <- aliens_Aus %>%
-  left_join(AlienTaxInfo_names, by = "Taxon")
+# AlienTaxInfo <- read.csv(file.path("SpeciesData", "AlienTaxInfo.csv"))
+# AlienTaxInfo <- as_tibble(AlienTaxInfo)
+# AlienTaxInfo_names <- AlienTaxInfo %>% 
+#   distinct(verbatimScientificName = verbatimScientificName, .keep_all = T) %>%
+#   rename(acceptedName = "scientificName") %>%
+#   rename(Taxon = "verbatimScientificName") %>%
+#   dplyr::select(Taxon, acceptedName)
+# aliens_Aus <- aliens_Aus %>%
+#   left_join(AlienTaxInfo_names, by = "Taxon")
 
 #Match the names
-# acc_alien_names <- unique(aliens_Aus$acceptedName)
-# gbif_taxon_keys <- acc_alien_names %>% #speciesNames = combination of harmonized range map and plant occurrence species (see RedList.R)
-#   taxize::get_gbifid_(method = "backbone") %>%
-#   imap(~ .x %>% mutate(original_sciname = .y)) %>%
-#   bind_rows() %T>%
-#   write.csv(file = file.path("SpeciesData", "alien_all_matches.csv")) %>% #can then examine the data
-#   filter(matchtype == "EXACT" & status == "ACCEPTED") %>%
-#   pull(usagekey)
+gbif_taxon_keys <- alien_names %>% #speciesNames = combination of harmonized range map and plant occurrence species (see RedList.R)
+  taxize::get_gbifid_(method = "backbone") %>%
+  imap(~ .x %>% mutate(original_sciname = .y)) %>%
+  bind_rows() %T>%
+  write.csv(file = file.path("SpeciesData", "alien_all_matches.csv")) %>% #can then examine the data
+  filter(matchtype == "EXACT" & status == "ACCEPTED") %>%
+  pull(usagekey)
 
 alien_matches <- read.csv(file = file.path("SpeciesData", "alien_all_matches.csv"))
 gbif_taxon_keys <- alien_matches %>% 
@@ -40,11 +40,17 @@ email <- "david.clarke1@monash.edu"
 
 #Request download
 #For these I should probably get all occurrence records from the world, enabling the IAS SDM if required.
+#Although GDM analysis for Aus wouldn't require this
 res <- occ_download(
   pred_in("taxonKey", gbif_taxon_keys),
   pred_in("basisOfRecord", c('HUMAN_OBSERVATION','OBSERVATION','MACHINE_OBSERVATION', 'PRESERVED_SPECIMEN')),
-  #pred("country", "AU"),
+  pred("country", "AU"),
   pred("hasCoordinate", TRUE),
   pred("hasGeospatialIssue", FALSE),
   format = "SIMPLE_CSV",
   user=user,pwd=pwd,email=email)
+
+#Download data - use path of an external hard drive
+#Download key: 0314789-200613084148143
+#Total records: 23572690
+occ_download_get("0314789-200613084148143", path = file.path("SpatialData", "Vector", "SpeciesOccurrences", "GBIF"))
