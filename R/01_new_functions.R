29720ae4129bb47ef4204472f210f2fc61673d61
David Clarke <david_anthony_clarke@hotmail.com>
2024-10-09 15:17:06 +1100

    Create 01_new_functions.R

---
+++ R/01_new_functions.R
@@ -0,0 +1,223 @@
+################################################################################
+## Script name: 01_functions.R
+## Author: David Clarke
+## Copyright (c) David Clarke, 2024
+## Email: david_anthony_clarke@hotmail.com
+################################################################################
+
+## Priority rank map----
+rank_plot <- function(rankmap){
+  
+  ras_st <- stars::st_as_stars(rankmap)
+  ras_sf <- st_as_sf(ras_st)
+  fill_ras_sf <- st_drop_geometry(ras_sf)
+  ggplot()+
+    geom_sf(data = ras_sf, aes(fill=fill_ras_sf[,1]), 
+            color=NA, 
+            show.legend = T) + 
+    scale_fill_gradientn(colours = z_colors_spectral$colors,
+                         values = z_colors_spectral$values,
+                         name = "Site\nsensitivity",
+                         breaks = c(0.0, 0.2, 0.4, 0.6, 0.8,1)) +
+    theme_bw() +
+    theme(axis.line = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.text = element_blank(),
+          panel.border = element_blank(),
+          axis.ticks = element_blank())
+  
+}
+
+## Priority rank difference map----
+rank_diff <- function(rankmap1, rankmap2){
+  
+  species_only_diff <- rankmap1 - rankmap2
+  names(species_only_diff) <- "rankmap"
+  
+  coolwarm_hcl <- colorspace::diverging_hcl(11,h = c(250, 10), c = 100, 
+                                            l = c(37, 88), power = c(0.7, 1.7))
+  species_only_diff_sf <- species_only_diff %>%
+    stars::st_as_stars() %>%
+    st_as_sf()
+  
+  ggplot()+
+    geom_sf(data = species_only_diff_sf, aes(fill=rankmap), 
+            color=NA, 
+            show.legend = T) + 
+    scale_fill_gradientn(colours = rev(coolwarm_hcl),
+                         name = "Sensitivity\ndifference",
+                         breaks = c(-0.5, 0.0, 0.5)) +
+    theme_bw() +
+    theme(axis.line = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.text = element_blank(),
+          panel.border = element_blank(),
+          axis.ticks = element_blank())
+  
+}
+
+## Calculate Jaccard similarities----
+calculate_jaccards <- function(rank_stack, x.min, x.max, y.min, y.max, 
+                               variant_names) {
+  
+  jaccards <- matrix(nrow = nlayers(rank_stack), ncol = nlayers(rank_stack))
+  for (i in 1:nrow(jaccards)) {
+    for (j in 1:ncol(jaccards)) {
+      if (i == j) {
+        jaccards[i, j] <- 1
+      }
+      else {
+        if (is.na(jaccards[j, i])) {
+          message(paste0("Calculating Jaccard index between ", 
+                         names(rank_stack[[i]]), 
+                         " and ", names(rank_stack[[j]])))
+          jaccards[i, j] <- jaccard(rank_stack[[i]], rank_stack[[j]], 
+                                    x.min = x.min, x.max = x.max, 
+                                    y.min = y.min, y.max = y.max)
+        }
+        else {
+          jaccards[i, j] <- NA
+        }
+      }
+    }
+  }
+  jaccards <- as.data.frame(jaccards)
+  colnames(jaccards) <- variant_names
+  rownames(jaccards) <- variant_names
+  return(jaccards)
+}
+
+## Raster correlation matrix----
+ras_cor <- function(ras_stack){
+  
+  rcors <- sapply(full_rank_stack, function(x) 
+            sapply(full_rank_stack, function(y) 
+              layerCor(c(x,y), "cor", asSample = F)))
+
+  rcors <- rcors[seq(1, length(rcors), 3)] #get every 3rd element (coefs)
+  mycors <- c()
+
+  for(i in 1:length(rcors)){
+  
+    v <- rcors[[i]][2,1]
+    mycors <- c(mycors,v)
+  
+  }
+
+  mymat <- round(matrix(data = mycors, 
+                        nrow = sqrt(length(mycors)), 
+                        ncol = sqrt(length(mycors)), 
+                        byrow = T),2)
+  
+  return(mymat)
+
+}
+
+## Calculate Jaccards
+jaccard <- function(x, y, x.min=0.0, x.max=1.0, y.min=0.0, y.max=1.0,
+                    warn.uneven=FALSE, limit.tolerance=4,
+                    disable.checks=FALSE) {
+  
+  if (!disable.checks) {
+    # Check the input values
+    x.min.value <- round(cellStats(x, stat="min"), limit.tolerance)
+    x.max.value <- round(cellStats(x, stat="max"), limit.tolerance)
+    y.min.value <- round(cellStats(y, stat="min"), limit.tolerance)
+    y.max.value <- round(cellStats(y, stat="max"), limit.tolerance)
+    
+    if (x.min < x.min.value) {
+      stop(paste0("Minimum threshold value for x ("), x.min, ") smaller than
+            the computed minimum value in x (", x.min.value, ")")
+    }
+    if (x.max > x.max.value) {
+      stop(paste0("Maximum threshold value for x ("), x.max, ") smaller than
+            the computed maximum value in x (", x.max.value, ")")
+    }
+    if (x.min >= x.max) {
+      stop(paste0("Minimum threshold value for x ("), x.min, ") smaller than
+             maximum threshold value for x (", x.max, ")")
+    }
+    if (y.min < y.min.value) {
+      stop(paste0("Minimum threshold value for y ("), y.min, ") smaller than
+            the computed minimum value in y (", y.min.value, ")")
+    }
+    if (y.max > y.max.value) {
+      stop(paste0("Maximum threshold value for y ("), y.max, ") smaller than
+            the computed maximum value in y (", y.max.value, ")")
+    }
+    if (y.min >= y.max) {
+      stop(paste0("Minimum threshold value for y ("), y.min, ") smaller than
+             maximum threshold value for y (", y.max, ")")
+    }
+    
+    # Comparisons using just the defaults is probably not feasible
+    if (x.min == 0.0 & x.max == 1.0 & y.min == 0.0 & y.max == 1.0) {
+      warning("Using all the defaults for x and y ranges")
+    }
+  } else {
+    message("Input limit checks disabled")
+  }
+  
+  # [fixme] - using cellStats(X, "sum") should be safe as we're dealing with
+  # binary 0/1 rasters. count() would be preferable, but apparently raster
+  # (>= 2.2 at least) doesn't support it anymore.
+  
+  # Get the values according to the limits provided
+  x.bin <- (x >= x.min & x <=x.max)
+  y.bin <- (y >= y.min & y <=y.max)
+  
+  if (warn.uneven) {
+    x.size <- cellStats(x.bin, "sum")
+    y.size <- cellStats(y.bin, "sum")
+    # Sort from smaller to larger
+    sizes <- sort(c(x.size, y.size))
+    if (sizes[2] / sizes[1] > 20) {
+      warning("The extents of raster values above the threshhold differ more",
+              "than 20-fold: Jaccard coefficient may not be informative.")
+    }
+  }
+  
+  # Calculate the intersection of the two rasters, this is given by adding
+  # the binary rasters together -> 2 indicates intersection
+  combination <- x.bin + y.bin
+  intersection <- combination == 2
+  
+  # Union is all the area covered by the both rasters
+  union <- combination >= 1
+  
+  return(cellStats(intersection, "sum") / cellStats(union, "sum"))
+}
+## Susceptible site prep
+susc_site_prep <- function(species_name, ras_stack){
+  
+  sp <- gsub(" ", ".", species_name)
+
+  bin <- rast(raster(here(regional_model_path, 
+                             sp, 
+                             "proj_regional", 
+                             "individual_projections", 
+                             paste0(sp,
+                                    "_EMcaByTSS_mergedAlgo_mergedRun_mergedData_TSSbin.gri"))))
+  
+  bin2 <- bin
+  bin2[bin2 != 0] <- 1
+  
+  # Re-class zeros as NA
+  bin[bin == 0] <- NA
+  
+  bin_list <- list()
+  
+  for(i in 1:nlyr(ras_stack)){
+  
+    bin_list[[i]] <- resample(bin, ras_stack[[i]], method = "near") 
+    names(bin_list[[i]]) <- names(ras_stack[[i]])
+    
+  }
+  
+  return(list(bin_list, bin2))
+
+}
