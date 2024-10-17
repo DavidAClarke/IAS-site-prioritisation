8c9c599e64eef38f83817253a72c510ab8a24dc1
David Clarke <david_anthony_clarke@hotmail.com>
2024-10-17 15:27:12 +1100

    Update 01_new_functions.R

--- R/01_new_functions.R
+++ R/01_new_functions.R
@@ -306,3 +306,124 @@ multi_props <- function(vals, props){
                     "0.00")
   return(diffs)
 }
+
+## Apply additional weighting schemes----
+spp_wght_fun <- function(df, scheme = 1, wv = c(1,2,4,6,8,2)){
+  
+  nm <- paste0("scheme_",scheme)
+  
+  # using {{}} := allows you to reference a column name as a string
+  #https://stackoverflow.com/questions/73285003/how-do-i-pass-a-column-name-to-a-function-involving-mutate
+  
+  if(scheme == 1 | scheme == 3){
+    
+    df1 <- df %>% 
+      
+      mutate({{nm}} := case_when(redlistCategory == "Least Concern" ~ wv[1],
+                                 redlistCategory == "Near Threatened" ~ wv[2],
+                                 redlistCategory == "Vulnerable" ~ wv[3],
+                                 redlistCategory == "Endangered" ~ wv[4],
+                                 redlistCategory == "Critically Endangered" ~ wv[5],
+                                 redlistCategory == "Data Deficient" ~ wv[6]))
+    
+    
+  }
+  if(scheme == 2 | scheme == 4){
+    
+    df1 <- df %>% 
+      
+      mutate({{nm}} := case_when(redlistCategory == "Least Concern" ~ wv[1],
+                                 redlistCategory == "Near Threatened" ~ wv[2],
+                                 redlistCategory == "Vulnerable" ~ wv[3],
+                                 redlistCategory == "Endangered" ~ wv[4],
+                                 redlistCategory == "Critically Endangered" ~ wv[5],
+                                 redlistCategory == "Data Deficient" ~ wv[6])) %>%
+      
+      mutate({{nm}} := case_when(code == "8.1.1" | code == "8.1.2" ~ get(!!nm) + 1,
+                                 .default = get(!!nm)))
+    
+  }
+  
+  return(df1)
+  
+}
+
+spp_area_wght_fun <- function(df, scheme = 1, wv = c(1,2,4,6,8,2)){
+  
+  nm <- paste0("scheme_",scheme)
+  
+  areas <- c("Ecosystem","Community","Ramsar","Upstream")
+  
+  if(scheme == 1 | scheme == 3){
+    
+    df1 <- df %>% 
+      
+      mutate({{nm}} := case_when(threatStatus == "Least Concern" &
+                                   !classGroup %in% areas ~ wv[1],
+                                 threatStatus == "Least Concern" &
+                                   classGroup %in% areas ~ wv[1] + 10 ,
+                                 threatStatus == "Near Threatened" &
+                                   !classGroup %in% areas ~ wv[2],
+                                 threatStatus == "Near Threatened" &
+                                   classGroup %in% areas ~ wv[2] + 10 ,
+                                 threatStatus == "Vulnerable" &
+                                   !classGroup %in% areas ~ wv[3],
+                                 threatStatus == "Vulnerable" &
+                                   classGroup %in% areas ~ wv[3] + 10 ,
+                                 threatStatus == "Endangered" &
+                                   !classGroup %in% areas ~ wv[4],
+                                 threatStatus == "Endangered" &
+                                   classGroup %in% areas ~ wv[4] + 10 ,
+                                 threatStatus == "Critically Endangered" &
+                                   !classGroup %in% areas ~ wv[5],
+                                 threatStatus == "Critically Endangered" &
+                                   classGroup %in% areas ~ wv[5] + 10 ,
+                                 threatStatus == "Data Deficient" &
+                                   !classGroup %in% areas ~ wv[6],
+                                 threatStatus == "Data Deficient" &
+                                   classGroup %in% areas ~ wv[6] + 10 ,
+                                 .default = weight)) #%>%
+    
+    #mutate({{nm}} := case_when(classGroup %in% areas ~ get(!!weight)))
+  }
+  if(scheme == 2 |scheme == 4){
+    
+    df1 <- df %>% 
+      
+      mutate({{nm}} := case_when(threatStatus == "Least Concern" &
+                                   !classGroup %in% areas ~ wv[1],
+                                 threatStatus == "Least Concern" &
+                                   classGroup %in% areas ~ wv[1] + 10 ,
+                                 threatStatus == "Near Threatened" &
+                                   !classGroup %in% areas ~ wv[2],
+                                 threatStatus == "Near Threatened" &
+                                   classGroup %in% areas ~ wv[2] + 10 ,
+                                 threatStatus == "Vulnerable" &
+                                   !classGroup %in% areas ~ wv[3],
+                                 threatStatus == "Vulnerable" &
+                                   classGroup %in% areas ~ wv[3] + 10 ,
+                                 threatStatus == "Endangered" &
+                                   !classGroup %in% areas ~ wv[4],
+                                 threatStatus == "Endangered" &
+                                   classGroup %in% areas ~ wv[4] + 10 ,
+                                 threatStatus == "Critically Endangered" &
+                                   !classGroup %in% areas ~ wv[5],
+                                 threatStatus == "Critically Endangered" &
+                                   classGroup %in% areas ~ wv[5] + 10 ,
+                                 threatStatus == "Data Deficient" &
+                                   !classGroup %in% areas ~ wv[6],
+                                 threatStatus == "Data Deficient" &
+                                   classGroup %in% areas ~ wv[6] + 10 ,
+                                 .default = weight)) %>%
+      
+      #mutate({{nm}} := case_when(classGroup %in% areas ~ weight)) %>%
+      
+      mutate({{nm}} := case_when(code == "8.1.1" | code == "8.1.2" ~ get(!!nm) + 1,
+                                 .default = get(!!nm)))
+    
+    
+  }
+  
+  return(df1)
+  
+}
